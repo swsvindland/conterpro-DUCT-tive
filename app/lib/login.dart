f69be01d19177b55c6b70b326b9725f0af3078bc
Sam Svindland <sam@Sams-MacBook-Pro.local>
2018-12-12 18:48:45 -0800

    add_login

---
+++ app/lib/login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Login'),
+      ),
+      body: new Container(
+        child: new Text('Hello World'),
+      ),
+    );
+  }
+}
